f4edcc84fd0975ed507067b0f312c0ee1024d483
20jun <ex_xe@naver.com>
2022-01-21 23:56:23 +0900

    #3.2

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.querySelector(".hello h1");
 
-console.log(isNaN(age));
-
-if (isNaN(age)) {
-  console.log("Please write a number");
-} else {
-  console.log("Thank you for writing your age.");
-}
+title.innerText = "hello";
